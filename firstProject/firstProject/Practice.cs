9d2fa380d298cb1bd6d92d672c3963b408af1d6c
Anubhav Kushwaha <anubhavkush8@gmail.com>
2025-09-01 10:46:13 +0530

    Learning

---
+++ firstProject/firstProject/Practice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstProject
+{
+    internal class Practice
+    {
+        internal static void fun1()
+        {
+            int y = 45;   // static 
+            Console.WriteLine("Value of y: " + y);
+        }
+
+        internal void fun2()
+        {
+            int z = 90;    // instance
+            Console.WriteLine("Value of z: " + z);
+        }
+    }
+
+    internal class Practice1
+    {
+        internal static void fun1()
+        {
+            int y = 45;   // static 
+            Console.WriteLine("Value of y: " + y);
+        }
+
+        internal void fun2()
+        {
+            int z = 90;    // instance
+            Console.WriteLine("Value of z: " + z);
+        }
+    }
+}
---
+++ firstProject/firstProject/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace firstProject
+{
+    internal class Program
+    {
+        string values = "Values all of the variables:";
+        internal static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.values);
+            int x = 5;
+            Console.WriteLine("Value of x: " + x);
+
+            //Calling static and instance method of Practice class
+            Practice obj1 = new Practice();
+            obj1.fun2();
+            Practice.fun1();
+
+            //Calling static and instance method of Practice-1 class
+            Practice1 obj2 = new Practice1();
+            obj2.fun2();
+            Practice.fun1();
+        }
+    }
+}
